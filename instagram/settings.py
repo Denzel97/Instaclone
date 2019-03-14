317f58f6d05e0b1e704f1d3129544683bafec407
Denzel97 <denzelwash97@gmail.com>
2019-03-14 18:04:24 +0300

    deployment

--- instagram/settings.py
+++ instagram/settings.py
@@ -154,7 +154,7 @@ from decouple import config,Csv
 
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG')
 # development
 if config('MODE')=="dev":
    DATABASES = {
